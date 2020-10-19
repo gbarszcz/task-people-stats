cadc488a44b54f1a4b0555fe5a469ea1dd0cafb2
Gabriela Barszcz <gabrielabarszcz2@gmail.com>
2020-10-19 18:02:20 +0200

    Added counting people and calling it from main method

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+		PeopleStats peopleStats = new PeopleStats(Paths.get("śćieżka", "do", "pliku"));
+
+		System.out.println(String.format("Liczba osób: %d", peopleStats.count()));
     }
 }
 
@@ -17,6 +19,10 @@ class PeopleStats {
             throw new IllegalStateException(e);
         }
     }
+	
+	public long count() {
+		return people.size();
+	}
 }
 
 class Person {
